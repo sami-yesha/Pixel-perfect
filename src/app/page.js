dc99955bb7806c6ca5e0b048ac09f55105e5491b
sami <samuelyeshambel9@gmail.com>
2024-08-09 19:12:27 +0300

    modify

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import CheckboxComponent from '@/components/CheckboxComponent';
 
 export default function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'white' }}>
       <CheckboxComponent />
     </div>
   );
